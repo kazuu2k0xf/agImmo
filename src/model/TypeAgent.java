bc138831653ac752cc305f433baf03e5bfe52e4f
Hugo-3_8 <hugomx@sfr.fr>
2024-12-23 15:45:28 +0100

    feat: Ajout des StringProperty

--- src/model/TypeAgent.java
+++ src/model/TypeAgent.java
@@ -1,5 +1,8 @@
 package model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class TypeAgent {
 
 	/**
@@ -8,8 +11,8 @@ public class TypeAgent {
 	 * typeAgentLbc	[String]	: Libellé court
 	 */
 	private int		typeAgentIdt;
-	private String	typeAgentLbl;
-	private String	typeAgentLbc;
+	private StringProperty	typeAgentLbl;
+	private StringProperty	typeAgentLbc;
 	/**
 	 * @param newTypeAgentIdt
 	 * @param newTypeAgentLbl
@@ -18,8 +21,8 @@ public class TypeAgent {
 	public TypeAgent(int newTypeAgentIdt, String newTypeAgentLbl, String newTypeAgentLbc) {
 		super();
 		this.typeAgentIdt = newTypeAgentIdt;
-		this.typeAgentLbl = newTypeAgentLbl;
-		this.typeAgentLbc = newTypeAgentLbc;
+		this.typeAgentLbl = new SimpleStringProperty(newTypeAgentLbl);
+		this.typeAgentLbc = new SimpleStringProperty(newTypeAgentLbc);
 	}
 	public int getTypeAgentIdt() {
 		return typeAgentIdt;
@@ -27,23 +30,41 @@ public class TypeAgent {
 	public void setTypeAgentIdt(int typeAgentIdt) {
 		this.typeAgentIdt = typeAgentIdt;
 	}
+	
 	public String getTypeAgentLbl() {
+		return typeAgentLbl.get();
+	}
+	public StringProperty getTypeAgentLblProperty() {
 		return typeAgentLbl;
 	}
-	public void setTypeAgentLbl(String typeAgentLbl) {
+	
+
+	public void setTypeAgentLblProperty(String typeAgentLbl) {
+		this.typeAgentLbl = new SimpleStringProperty(typeAgentLbl);
+	}
+	public void setTypeAgentLbl(StringProperty typeAgentLbl) {
 		this.typeAgentLbl = typeAgentLbl;
 	}
+	
+	
 	public String getTypeAgentLbc() {
+		return typeAgentLbc.get();
+	}
+	public StringProperty getTypeAgentLbcProperty() {
 		return typeAgentLbc;
 	}
+	
 	public void setTypeAgentLbc(String typeAgentLbc) {
-		this.typeAgentLbc = typeAgentLbc;
+		this.typeAgentLbc = new SimpleStringProperty(typeAgentLbc);
+	}
+	public void setTypeAgentLbcProperty(StringProperty typeAgentLbc) {
+		this.typeAgentLbc = (typeAgentLbc);
 	}
 	
 	
 	@Override
 	public String toString() {
-	    return typeAgentLbc; 
+	    return typeAgentLbc.get(); 
 	}
 	
 	
